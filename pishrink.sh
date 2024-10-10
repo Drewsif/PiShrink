d5c162ccd99b62c95a3f5c880e6aa4e21e0f5cab
Drew Bonasera <Drewsif@users.noreply.github.com>
2024-10-10 02:47:36 -0400

    Bump version, also swapping to date based :D

--- pishrink.sh
+++ pishrink.sh
@@ -4,7 +4,7 @@
 # Description: PiShrink is a bash script that automatically shrink a pi image that will then resize to the max size of the SD card on boot.
 # Link: https://github.com/Drewsif/PiShrink
 
-version="v0.1.5"
+version="v24.10.10"
 
 CURRENT_DIR="$(pwd)"
 SCRIPTNAME="${0##*/}"
