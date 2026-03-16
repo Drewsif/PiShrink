a5f9463c01607ab07402c7e75c9cfd4bb3a0e886
Drew Bonasera <Drewsif@users.noreply.github.com>
2026-03-16 05:17:17 -0400

    Bumps script version and updates the copyright year in the LICENSE file.
    - chore: Bump version to v26.03.16
    - docs(license): Update copyright year to 2026

--- pishrink.sh
+++ pishrink.sh
@@ -4,7 +4,7 @@
 # Description: PiShrink is a bash script that automatically shrink a pi image that will then resize to the max size of the SD card on boot.
 # Link: https://github.com/Drewsif/PiShrink
 
-version="v24.10.23"
+version="v26.03.16"
 startSeconds=$SECONDS
 
 CURRENT_DIR="$(pwd)"
